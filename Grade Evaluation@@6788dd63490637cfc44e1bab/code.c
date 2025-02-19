2594ba8d91628fecce3ff3422eac6c407015339d
Ishika Singla <ishikasingla30@gmail.com>
2025-02-19 19:59:25 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    char day;
+    scanf("%c", &day);
+    if(day==A) {
+        printf("Excellent");
+    }
+    else if(day==B) {
+        printf("Good");
+    }
+    else if(day==C) {
+        printf("Average");
+    }
+    else if(day==D) {
+        printf("Below Average");
+    }
+    else if(day==F) {
+        printf("Fail");
+    }
+    else {
+        printf("Invalid grade");
+    }
+    return 0;
+    
+}
