2905fcd490fb5e793b35d5e82fb85730737c58d9
Ishika Singla <ishikasingla30@gmail.com>
2025-03-17 20:30:57 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,10 +4,10 @@ int main() {
     int a, b;
     char op;
 
-    // Taking input: two integers and an operator
+   
     scanf("%d %d %c", &a, &b, &op);
 
-    // Performing operation based on the operator
+    
     switch (op) {
         case '+':
             printf("%d\n", a + b);
@@ -20,13 +20,13 @@ int main() {
             break;
         case '/':
             if (b == 0) {
-                printf("Error\n");  // Handling division by zero
+                printf("Error\n");  
             } else {
                 printf("%d\n", a / b);
             }
             break;
         default:
-            printf("Error\n");  // Handling invalid operator
+            printf("Error\n");  
     }
 
     return 0;
